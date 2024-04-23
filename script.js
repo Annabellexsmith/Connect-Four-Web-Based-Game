165b7c423372506e03c2bcc4e5adf4ad6e30fc90
AnnabellexSmith <sannabelle056@gmail.com>
2024-04-23 12:06:00 -0700

    dropToken alert dialog working

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let board;
 let player;
 let winner;
 let tie; 
+let messageEl = document.getElementById('turn')
 
 // CONSTANTS --------------------------------------------------------
 const winningCombos =[[0, 1, 2, 3], [1, 2, 3, 4], [5, 6, 7, 8], [6, 7, 8, 9],
@@ -30,19 +31,18 @@ const token = {
 // CATCHED ELEMENTS -------------------------------------------------
 const openModal = document.getElementById('instruction-modal')
 const playButtonEl = document.getElementById('play-game')
-let messageEl = document.getElementById('turn')
 const slotEls = document.querySelectorAll('.slot')
+const alertModal = document.getElementById('alertModal')
+const tryAgainButton = document.getElementById('tryAgainButton')
 const resultModal = document.getElementById('end-game-modal')
 const endMessageEl = document.getElementById('end-message')
 const restartButtonEl = document.getElementById('restart')
 
-// all catched correctly
-
 // EVENT LISTENERS --------------------------------------------------
 playButtonEl.addEventListener("click", closeButton)
 slotEls.forEach(function (slotEl){slotEl.addEventListener("click", dropToken)})
+tryAgainButton.addEventListener("click", closeAlertModal)
 restartButtonEl.addEventListener("click", restart)
-
 
 // FUNCTIONS ---------------------------------------------------------
 function init () {
@@ -56,52 +56,47 @@ init ()
 function closeButton() {openModal.close()}
 
 function dropToken(event) { // return to this!! 
-  //  console.log(parseInt(event.target.innerText))
     let pickedSlot = parseInt(event.target.id);
-    console.log(pickedSlot, board[pickedSlot])
-        
-           // if (board[i] !== null || winner) return; // if there is a tie or winner return 
             if(pickedSlot <= 4 && board[pickedSlot] === null) {
                 event.target.innerHTML= token[player]
                 board[pickedSlot] = player
             } else if (board[pickedSlot - 5] !== null) {
-               // console.log(board[i - 5], i)
                 event.target.innerHTML = token[player]
                 board[pickedSlot] = player
             } else {
-                alert("invalid selection, please pick a spot with a token underneath it") 
+                alertModal.show()
             }
-            
         
-        //update board array to match the visual board
-console.log(board)
-    //(!== null && board[i] === null)
-    // assessForWinner()
+    //  assessForWinner();
      player *= -1;
 }
 
-function assessForWinner() {
-    for(let i = 0; i < winningCombos.length; i++) {
-        const slotOne = board[winningCombos[i][0]];
-        const slotTwo = board[winningCombos[i][1]];
-        const slotThree = board[winningCombos[i][2]];
-        const slotFour  = board[winningCombos[i][3]];
-        const slotFive = board[winningCombos[i][4]]
-     if (slotOne === slotTwo && slotTwo === slotThree && slotThree === slotFour && slotFour === slotFive) {
-        return resultModal.showModal("win") // should that be up here?
-        } else if (board[i] !== null) return resultModal.showModal("tie")
-    }
-}
+function closeAlertModal () {alertModal.close()}
+
+// function assessForWinner(winner) {
+//     winner = false;
+//     for(let i = 0; i < winningCombos.length; i++) {
+//         const slotZero = board[winningCombos[i][0]];
+//         const slotOne = board[winningCombos[i][1]];
+//         const slotTwo = board[winningCombos[i][2]];
+//         const slotThree  = board[winningCombos[i][3]];
+//         const slotFour = board[winningCombos[i][4]]
+//         if (slotZero === "1" && slotOne === "1" && slotTwo === "1" && slotThree === "1" && slotFour === "1") {
+//            winner = "win";
+//             return resultModal.showModal() 
+//         } else {(board[i] !== null) 
+//             return resultModal.showModal("tie")
+//         }
+//     }
+// }
 
 function message() { //can we merge this one with determWinner
     if (winner === "win") endMessageEl.innerText = `Congratulations ${player}! You have won! Play again?`
     else endMessageEl.innerText = `Gasp! It's a tie. Only way to figure out who wins is to play again!`
 }
 
+
 function restart() {
     resultModal.close();
     init ()
     }
-    //const pickedSlot = parseInt(event.target.id);
-    // if (board[pickedSlot] !== null || winner) return // and there the spot under it is not empty
-    // board[pickedSlot] = player;
