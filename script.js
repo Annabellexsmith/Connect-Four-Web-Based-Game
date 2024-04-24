867d49a08778a19974cb7a9c9600eb3285cfc6ec
AnnabellexSmith <sannabelle056@gmail.com>
2024-04-23 20:37:18 -0700

    reset button working

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// onload, have the instruction modal open 
-//Listen event for the start button and call the funciton init to begin the game
-//init function should include any empty board, start with player 1, have winner set to false, and message saying that is is the first persons turn
-//function that represents everything that happens in one turn: event lisener, deterimine if the spot if available then place the token, check board against the winning combo array, and, change player, change message to who's turn it is
-//for loop function that runs between the board & winning array
-//function for messages based on who wins then calls on the footer modal, tie, whos' turn
-//addlistener for play again button with callback to the init function
-
 // VARIABLES --------------------------------------------------------
 let board;
 let player;
@@ -15,7 +7,9 @@ let winner;
 const winningCombos = [[0, 1, 2, 3], [1, 2, 3, 4], [5, 6, 7, 8], [6, 7, 8, 9],
 [10, 11, 12, 13], [11, 12, 13, 14], [15, 16, 17, 18], [16, 17, 18, 19],
 [20, 21, 23, 23], [21, 22, 23, 24], [1, 7, 13, 19], [0, 6, 12 ,18], 
-[6, 12, 18, 24], [5, 11, 17, 23]];
+[6, 12, 18, 24], [5, 11, 17, 23], [0, 5, 10, 15, ], [5, 10, 15, 20], 
+[1, 6, 11, 16], [6, 11, 16, 21], [2, 7, 12, 17], [7, 12, 17, 22], 
+[3, 8, 13, 18], [8, 13, 18, 23], [4, 9, 14, 19], [9, 14, 19, 24]]
 
 const LOOKUP = {
     "1": "Player One",
@@ -38,10 +32,13 @@ const resultMessageEl = document.getElementById('end-message')
 const restartButtonEl = document.getElementById('restart')
 
 // EVENT LISTENERS --------------------------------------------------
-playButtonEl.addEventListener("click", closeButton)
-slotEls.forEach(function (slotEl){slotEl.addEventListener("click", dropToken)})
-tryAgainButton.addEventListener("click", closeAlertModal)
-restartButtonEl.addEventListener("click", restart)
+playButtonEl.addEventListener("click", function () {instructionEl.close()})
+slotEls.forEach(function (slotEl) {slotEl.addEventListener("click", dropToken)})
+tryAgainButton.addEventListener("click", function () {alertModal.close()})
+restartButtonEl.addEventListener("click", function() {
+    resultModal.close()
+    init()
+})
 
 // FUNCTIONS ---------------------------------------------------------
 function init () {
@@ -50,54 +47,55 @@ function init () {
     player = "1";
     winner = false;
     messageEl.innerText = `It is ${LOOKUP[player]}'s turn`;
+    render()
 }
 init ()
-function closeButton() {instructionEl.close()}
+
+function render() { //(board updating function)
+    board.forEach(function(text, index) {
+        const slotEl = document.getElementById(index);
+        if (!text) slotEl.innerText = ''
+    })
+};
 
 function dropToken(event) {
     let pickedSlot = parseInt(event.target.id);
-    console.log("board at pickedslot", board[pickedSlot])
-            if (board[pickedSlot] !== null) {
-                return;
-            } else if ((pickedSlot <= 4 && board[pickedSlot] === null) || board[pickedSlot - 5] !== null) {
-                event.target.innerHTML= token[player];
-                board[pickedSlot] = player;
-            } else {
-                alertModal.show();
-            }
-     console.log("updated board", board)
-     assessForWinner();
-     player *= -1;
-     messageEl.innerText = `It is ${LOOKUP[player]}'s turn`;
-}
-function closeAlertModal() {alertModal.close()}
-
-function assessForWinner(message) {
-    for(let i = 0; i < winningCombos.length; i++) {
-        const slotZero = board[winningCombos[i][0]];
-        const slotOne = board[winningCombos[i][1]];
-        const slotTwo = board[winningCombos[i][2]];
-        const slotThree  = board[winningCombos[i][3]];
-        const slotFour = board[winningCombos[i][4]]
-        if (slotZero === slotOne && slotOne === slotTwo && slotTwo === slotThree && slotThree === slotFour) {
-            winner = "win";
-            resultModal.openModal()
-            return message("win")
-        } else if (!board.includes(null)) {
-            resultModal.openModal()
-            return message()
-        } else {
-            continue;
+    if (board[pickedSlot] !== null) {
+         return;
+    } else if ((pickedSlot <= 4 && board[pickedSlot] === null) || board[pickedSlot - 5] !== null) {
+        event.target.innerText= token[player];
+        board[pickedSlot] = player;
+        winner = assessForWinner()
+        if (winner === "win" || winner === "tie") {
+            message();
+            resultModal.showModal();
+            return;
+        } else if (winner === null) {
+            player *= -1;
+            messageEl.innerText = `It is ${LOOKUP[player]}'s turn`;
         }
+    } else {
+        alertModal.show();
     }
+} 
+
+function assessForWinner () {
+    for(let i = 0; i < winningCombos.length; i++) {
+        if (Math.abs(board[winningCombos[i][0]] + board[winningCombos[i][1]]+ board[winningCombos[i][2]] + board[winningCombos[i][3]]) === 4) {
+            return "win"
+        } 
+    } 
+    if (!board.includes(null)) {
+        return "tie"
+    }  return null
 }
 
 function message() {
-    if (winner === "win") resultMessageEl.textContent = `Congratulations ${LOOKUP[player]}! You have won! Play again?`
+    if (winner === "win") resultMessageEl.textContent = `Congratulations ${LOOKUP[player *= -1]}! You have won! Play again?`
     else resultMessageEl.textContent = `Gasp! It's a tie. Only way to figure out who wins is to play again!`
 }
 
+
 function restart() {
-    resultModal.close();
-    init ()
+    resultModal.close()
     }
